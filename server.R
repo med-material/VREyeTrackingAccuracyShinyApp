18c0d9e0f41f07937b4f260d3082739125ccf3fb
YoNeXia <yohann.neraud@viacesi.fr>
2019-11-15 10:55:15 +0100

    Initial files to Shiny app
    Add files to create and control a Shiny app.
    Add libraries called by the app in each file.

---
+++ server.R
@@ -0,0 +1,5 @@
+##### Execute methods from global.R to render in ui.R #####
+
+# Libraries called
+library(shiny)
+library(RMySQL)
---
+++ ui.R
@@ -0,0 +1,5 @@
+##### Control the layout and appearance #####
+
+# Libraries called
+library(shiny)
+library(RMySQL)
