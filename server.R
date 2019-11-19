9e6956da239c4a3e342abd04528415e3b773b5c1
YoNeXia <yohann.neraud@viacesi.fr>
2019-11-19 14:38:41 +0100

    Delete query in UI
    Remove code to show the actual query in UI.

--- server.R
+++ server.R
@@ -6,25 +6,6 @@ library(RMySQL)
 
 # Server method
 server = function(input, output) {
-  # Render active query
-  render = reactive({
-    # Nothing selected
-    if (input$target == "All Targets" && input$participant == "All Participants") {
-      queryBuilder(query_number = 4)
-    }
-    # Only participant selected
-    else if (input$target == "All Targets") {
-      queryBuilder(query_number = 3, participant = input$participant)
-    }
-    # Only target selected
-    else if (input$participant == "All Participants") {
-      queryBuilder(query_number = 1, target = input$target)
-    }
-    # Target and participant selected
-    else {
-      queryBuilder(query_number = 2, target = input$target, participant = input$participant)
-    }
-  })
   # Render a circle
   renderCircle = reactive({
     # Nothing selected
